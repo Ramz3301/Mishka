998844875945380e0594326d873d19ca4ba3b4f5
ramzan <ramkurbanov10@yandex.ru>
2021-08-29 11:06:10 +0300

    Прижми ковёр,добавь интерактивную карту и js

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,22 @@
 let pageHeader = document.querySelector('.page-header');
-      let headerToggle = document.querySelector('.page-header__toggle');
+let headerToggle = document.querySelector('.page-header__toggle');
 
       // pageHeader.classList.remove('page-header--nojs')
 
-      headerToggle.addEventListener('click', function() {
-        if (pageHeader.classList.contains('page-header--closed')) {
-          pageHeader.classList.remove('page-header--closed');
-          pageHeader.classList.add('page-header--opened');
-        } else {
-          pageHeader.classList.add('page-header--closed');
-          pageHeader.classList.remove('page-header--opened');
-        }
-      });
+headerToggle.addEventListener('click', function() {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+  } else {
+      pageHeader.classList.add('page-header--closed');
+      pageHeader.classList.remove('page-header--opened');
+    }
+});
+
+const orderButton = document.querySelector('.product-of-the-week__button');
+const orderForm = document.querySelector('.modal');
+
+  orderButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    orderForm.classList.toggle('modal__show');
+  });
