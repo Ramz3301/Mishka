9a413e1128d122a7c2b9413b06d13b5794ac34bb
ramzan <ramkurbanov10@yandex.ru>
2021-09-01 10:12:19 +0300

    Измени одинарные кавычки на двойные

--- source/js/script.js
+++ source/js/script.js
@@ -1,30 +1,30 @@
-let pageHeader = document.querySelector('.page-header');
-let headerToggle = document.querySelector('.page-header__toggle');
+let pageHeader = document.querySelector(".page-header");
+let headerToggle = document.querySelector(".page-header__toggle");
 
       // pageHeader.classList.remove('page-header--nojs')
 
-headerToggle.addEventListener('click', function() {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+headerToggle.addEventListener("click", function() {
+  if (pageHeader.classList.contains("page-header--closed")) {
+    pageHeader.classList.remove("page-header--closed");
+    pageHeader.classList.add("page-header--opened");
   } else {
-      pageHeader.classList.add('page-header--closed');
-      pageHeader.classList.remove('page-header--opened');
+      pageHeader.classList.add("page-header--closed");
+      pageHeader.classList.remove("page-header--opened");
     }
 });
 
-const orderButton = document.querySelector('.product-of-the-week__button');
-const orderForm = document.querySelector('.modal');
+const orderButton = document.querySelector(".product-of-the-week__button");
+const orderForm = document.querySelector(".modal");
 
-  orderButton.addEventListener('click', function (evt) {
+  orderButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    orderForm.classList.toggle('modal-show');
+    orderForm.classList.toggle("modal-show");
   });
 
-const orderCart = document.querySelector('.catalog__price--link');
+const orderCart = document.querySelector(".catalog__price--link");
 
 
-  orderCart.addEventListener('click', function (evt) {
+  orderCart.addEventListener("click", function (evt) {
     evt.preventDefault();
-    orderForm.classList.toggle('modal-show');
+    orderForm.classList.toggle("modal-show");
   });
